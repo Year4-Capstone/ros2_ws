b5899d6a4bb7b65576c4743bc0b747b3bbd99c86
callumberry <callumberry281@gmail.com>
2025-12-27 15:34:21 -0500

    Fixed loading meshes and worlds
    Tested with cad_urdf urdf.
    Tested with empty_world.sdf and cave_world.world.
    
    small_house.world is still broken.

--- src/robot_sim_description/launch/view_robot_gazebo.launch.py
+++ src/robot_sim_description/launch/view_robot_gazebo.launch.py
@@ -1,50 +1,52 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_path
 from launch.substitutions import PathJoinSubstitution, Command, LaunchConfiguration
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import TextSubstitution
-from launch_ros.substitutions import FindPackageShare
-from launch.actions import AppendEnvironmentVariable
-from launch.actions import GroupAction
 
 def generate_launch_description():
-    world_name_arg = DeclareLaunchArgument(
-        'world_name',
-        default_value='cave_world.world',
-        description='World name (must match worlds/<name>.world and models/<name>/)'
-    )
 
-    world_name = LaunchConfiguration('world_name')
-
+    pkg_robot_sim_description = get_package_share_directory('robot_sim_description')    
     pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
-    gz_launch_path = PathJoinSubstitution([
-        pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py'
-    ])
+        
+    gz_launch_path = PathJoinSubstitution([pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py'])
 
-    pkg_desc = get_package_share_directory('robot_sim_description')
+    # world argument    
+    world_name_arg = DeclareLaunchArgument(
+        'world',
+        default_value='empty_world.sdf',
+        description='World to load: small_house.world, cave_world.world, or empty_world.sdf'
+    )
+    world_name = LaunchConfiguration('world')
 
+    # Construct the path to the world file
     world_path = PathJoinSubstitution([
-        pkg_desc,
+        pkg_robot_sim_description,
         'worlds',
         world_name
     ])
+    
+    # Get the package share directory for meshes
+    robot_sim_description_parent_path = os.path.dirname(pkg_robot_sim_description)
+    model_dir = os.path.join(pkg_robot_sim_description, 'models')
+    combined_resource_path = f"{robot_sim_description_parent_path}:{model_dir}"
+    
+    default_paths = [
+        os.path.expanduser("~/.gz/models"),
+        "/usr/share/gz/models",
+    ]
 
-    urdf_path = os.path.join(
-        pkg_desc, 'urdf', 'cad_urdf.urdf.xacro'
-    )
+    combined = ":".join(default_paths + [combined_resource_path])
 
-    set_gz_resource_path = AppendEnvironmentVariable(
+    set_gz_resource_path = SetEnvironmentVariable(
         name='GZ_SIM_RESOURCE_PATH',
-        value=PathJoinSubstitution([
-            FindPackageShare('robot_sim_description'),
-            'models'
-        ])
+        value=combined
     )
+
+    urdf_path = os.path.join(get_package_share_path('robot_sim_description'), 'urdf', 'cad_urdf.urdf.xacro')
 
     robot_description = {
         'robot_description': Command([
@@ -58,11 +60,6 @@ def generate_launch_description():
             'gz_args': ['-r -v 4 ', world_path]
         }.items()
     )
-
-    gazebo_group = GroupAction([
-        set_gz_resource_path,
-        gazebo,
-    ])
 
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
@@ -95,7 +92,8 @@ def generate_launch_description():
 
     return LaunchDescription([
         world_name_arg,
-        gazebo_group,
+        set_gz_resource_path, 
+        gazebo,
         robot_state_publisher_node,
         spawn_node,
         bridge_node,
